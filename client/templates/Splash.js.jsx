91ae933cce3080ca4d1878393a8d649127b3fa72
tgoldenberg <thomasagoldenberg@gmail.com>
2015-09-09 11:26:13 -0400

    create basic splash page for chess site

---
+++ client/templates/Splash.js.jsx
@@ -0,0 +1,14 @@
+var Splash = ReactMeteor.createClass({
+	templateName: "landingContent",
+
+	render: function() {
+		return (
+				<div className="content-holder">
+					<h1>ChessMentor</h1><br/><br/>
+					<button>PLAY</button>
+				</div>
+
+
+		)
+	}
+})
--- client/templates/Splash.jsx
+++
@@ -1,9 +0,0 @@
-var Splash = ReactMeteor.createClass({
-	templateName: "splash",
-
-	render: function() {
-		return (
-			<h1>Splash</h1>
-		)
-	}
-})
