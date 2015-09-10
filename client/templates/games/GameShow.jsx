2c45cec9bdb1ea1adb984b768dea510d60132af0
tgoldenberg <thomasagoldenberg@gmail.com>
2015-09-10 11:10:01 -0400

    add validation to make draggable only if user is a player

--- client/templates/games/GameShow.jsx
+++ client/templates/games/GameShow.jsx
@@ -7,11 +7,19 @@ var GameShow = ReactMeteor.createClass({
       currentUser: Meteor.user()
     };
   },
+  isPlayer: function() {
+    var userId = Meteor.userId() ? Meteor.userId() : Session.get('userId');
+    var blackId = this.props.black ? this.props.black.userId : null;
+    var whiteId = this.props.white ? this.props.white.userId : null;
+    return blackId == userId || whiteId == userId;
+  },
   componentDidMount: function() {
+    console.log(this.isPlayer());
+    var draggable = this.isPlayer();
     var cfg = {
       pieceTheme: '/{piece}.png',
       position: 'start',
-      draggable: true
+      draggable: draggable
     };
     var board1 = ChessBoard('board', cfg);
   },
